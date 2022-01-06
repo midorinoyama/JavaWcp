b1072e7750578aee14a12573322c315511b724fb
midorinoyama <fujiaqa@gmail.com>
2022-01-06 22:05:23 +0900

    test commit

---
+++ src/Null01.java
@@ -0,0 +1,19 @@
+
+public class Null01 {
+	public static void main(String[] args) {
+		String string1;
+		string1 = "あいう";
+		System.out.println(string1 + "の文字数：" + string1.length());
+		
+		// ””（空文字）の文字数を出力
+		string1 = "";
+		System.out.println(string1 + "の文字数：" + string1.length());
+		
+		
+		// nullの変数をlengthメソッドで参照するとエラー
+		//　メソッドなければ空白でnullがそのまま出力される
+		string1 = null;
+		//System.out.println(string1 + "の文字数：" + string1.length());
+	}
+
+}
