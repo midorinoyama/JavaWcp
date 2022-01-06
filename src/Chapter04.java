0eb00b6473712a90178989ce5a5b1b2fbc489ef4
midorinoyama <fujiaqa@gmail.com>
2022-01-06 18:34:48 +0900

    First commit

---
+++ src/Chapter04.java
@@ -0,0 +1,15 @@
+
+public class Chapter04 {
+
+	public static void main(String[] args) {
+		String name = "KANAE";
+		System.out.println(name);
+		
+		name = "nemui";
+        System.out.println(name);
+        
+        int number = 3;
+        System.out.println(number);
+	}
+
+}
---
+++ src/Ready02.java
@@ -0,0 +1,11 @@
+// クラスブロックの始まり
+public class Ready02 {
+	// メソッドブロックの始まり
+	public static void main(String[] args) {
+		// メソッドの処理
+		System.out.println("Hellow WEBCAMP!");
+		System.out.println("Hello Java!");
+	}
+	//メソッドブロックの終わり
+}
+// クラスブロックの終わり
