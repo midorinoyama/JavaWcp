6642b9a2037b92df27cf1edf9f560c05bbb0cc11
midorinoyama <fujiaqa@gmail.com>
2022-01-14 21:46:29 +0900

    Add 配列for文

--- src/Array02.java
+++ src/Array02.java
@@ -2,24 +2,46 @@
 public class Array02 {
 
 	public static void main(String[] args) {
-		// 偶数の和、奇数の和
+		// 配列の拡張for文　　偶数の和、奇数の和
 		//奇数の和
 		int oddSum = 0;
 		//偶数の和
 		int evenSum = 0;
-		
-		int[] numbers = {1,2,3,4,5,6,7,8,9,10};
-		
-		for (int i:numbers) {
-			if (i %2 == 0) {
-				evenSum = evenSum + i;
-		    } else {
-		    	oddSum += i;
-		    }
+//		
+//		int[] numbers = {1,2,3,4,5,6,7,8,9,10};
+//		
+//		for (int i:numbers) {
+//			if (i %2 == 0) {
+//				evenSum = evenSum + i;
+//		    } else {
+//		    	oddSum += i;
+//		    }
+//		}
+//
+		//配列のfor文
+		int[] numbers = {1,2,3};
+		//iはインデックス番号、要素数
+		for (int i = 0; i < numbers.length; i++) {
+			if (numbers[i] % 2 == 0) {
+				evenSum += evenSum + numbers[i];				
+			} else {
+				oddSum += numbers[i];
+			}
 		}
+		
 
-		    System.out.println("奇数の和は" + oddSum);
-		    System.out.println("偶数の和は" + evenSum);
+//		//配列なし通常のfor文		
+//		for (int i = 1; i <= 10; i++ ) {
+//			if (i % 2 == 0) {
+//				evenSum += i;
+//			} else {
+//				oddSum += i;
+//			}
+//		}
+		System.out.println("奇数の和は" + oddSum);
+	    System.out.println("偶数の和は" + evenSum);
+		
+		
 	}
 
 }
